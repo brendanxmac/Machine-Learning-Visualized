5dc1265cc9d257979f63e1ec5e4efa391dd24e77
Brendan <bmcla435@mail.endicott.edu>
2018-05-04 10:32:21 -0400

    logistic regression animations

--- logistic_regression.js
+++ logistic_regression.js
@@ -5,13 +5,15 @@ var x_min = 100;
 var x_max = 600;
 var y_min = 0;
 var y_max = 400;
-var num_training_data =50;
+var num_training_data =70;
 var training_data = [];
+var testing_data = [];
+var color = ' ';
 
 // Sigmoid Variables
-var b_0 = .5;
-var alpha = .000000001;
-var b_1 = .5;
+var b_0 = .09;
+var b_1 = .09;
+var alpha = .000001;
 var x_0 = 1;
 var iterations = 1000;
 var z;
@@ -21,18 +23,31 @@ var svgns = "http://www.w3.org/2000/svg";
 var container = document.getElementById('graph-log');
 
 for (var i = 0; i < num_training_data/2; i++) {
-  training_data.push([Math.floor(Math.random() * (360-110)) + 110, 400]);
+  // training_data.push([Math.floor(Math.random() * (360-110)) + 110, 400]);
+  training_data.push([(Math.random() * (2.5-0.0)).toFixed(3), 0]);
 }
 
 for (var i = 0; i < num_training_data/2; i++) {
-  training_data.push([Math.floor(Math.random() * (600-320)) + 320, 5]);
+  // training_data.push([Math.floor(Math.random() * (600-320)) + 320, 5]);
+  training_data.push([(Math.random() * (5-2.5) + 2.5).toFixed(3), 1]);
 }
+
+// DO 'THE' CONVERSIONS HERE
+// x = x+1 *100
+// y = 400 - (y * 395)
 
 //******************************************
 // Create Circles Function
 //******************************************
-function createCirlce(id, cx, cy, r, color, opacity) {
+function createCirlce(line, id, cx, cy, r, color, opacity) {
   var newPoint = document.createElementNS(svgns, 'circle');
+  if (line == 1) {
+    cx = (((cx+1) * 100) + 100).toFixed(2);
+    cy = 400 - (cy * 395);
+  } else {
+    cx = cx + 350;
+    cy = 400 - (cy * 395);
+  }
   newPoint.setAttributeNS(null, 'id', id);
   newPoint.setAttributeNS(null, 'cx', cx);
   newPoint.setAttributeNS(null, 'cy', cy);
@@ -57,14 +72,27 @@ function createLine(id, x1, x2, y1, y2, color, width, opacity, dash) {
 
 // create a DOM for each data point
 for (point in training_data) {
-  createCirlce(point, training_data[point][0], training_data[point][1], 5, 'black', .5);
+  if (training_data[point][1] == 0) {
+    color = 'green';
+  } else {
+    color = 'purple';
+  }
+  createCirlce(1, 'point'+point, training_data[point][0], training_data[point][1], 5, color, .7);
+  var plot_data = anime({
+    targets: '#' + 'point' + point,
+    r: {
+      value: 1,
+      easing: 'easeInOutSine',
+      duration: 500
+    },
+    direction: 'alternate'
+  });
 }
 
 //plot decision boundary
-createLine("decision_boundary", x_min, x_max, (y_max/2), (y_max/2), 'blue', 1, .4, 3);
+createLine("decision_boundary", x_min, x_max, (y_max/2), (y_max/2), 'blue', 1, .4, 1);
 
 function sigmoid(z) {
-  // console.log("Sigmoid Value: " + (1 / (1 + Math.exp(-z))));
   return (1/(1+Math.exp(-z)));
 }
 // Formula for Gradient Ascent
@@ -80,12 +108,9 @@ function plotSigmoid() {
   for (var i = 0; i < iterations; i++) {
     changes = 0;
     for (var j = 0; j < num_training_data; j++) {
-      // console.log("Point " + j + ":  x = " + training_data[j][0] + "  y = " + training_data[j][1]);
       z = b_0 + (b_1 * training_data[j][0]);
       var new_b_0 = b_0 + (alpha * (sigmoid(z) - training_data[j][1]) * x_0);
       var new_b_1 = b_1 + (alpha * (sigmoid(z) - training_data[j][1]) * training_data[j][0]);
-      // console.log("B_0: " + new_b_0);
-      // console.log("B_1: " + new_b_1);
       if (new_b_0 != b_0 || new_b_1 != b_1) {
         changes++;
       }
@@ -95,14 +120,28 @@ function plotSigmoid() {
     if (changes == 0) {
       break;
     }
-    console.log(i);
   }
   console.log("B_0: " + b_0);
   console.log("B_1: " + b_1);
+  for (var i = -250; i < x_max/2; i+=2) {
+    z = b_0 + (b_1 * i);
+    createCirlce(0, "sigmoid_point" + i, i, sigmoid(z), 20, 'orange', 1);
+
+    var plotSig = anime({
+      targets: '#' + 'sigmoid_point' + i,
+      r: {
+        value: 2,
+        easing: 'easeInOutSine',
+        duration: 300,
+      },
+    });
+  }
 }
 
 function plotNewData() {
-
+  var x_val = (Math.random() * (5.0-0.0)).toFixed(3);
+  z = b_0 + (b_1 * x_val);
+  createCirlce(1, "testing_data", x_val, sigmoid(z), 7, 'red', .7);
 }
 
 function classify() {
